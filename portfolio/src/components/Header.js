132dedcd19fdaba547def6f216530035014d1ab3
manish <rrew1212@gmail.com>
2023-05-18 01:37:40 +0530

    nav bar done

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,37 +1,82 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import './Header.css'
+import React, { useState } from "react";
+import "./Header.css";
+import {
+  FaFacebookSquare,
+  FaInstagramSquare,
+  FaYoutubeSquare,
+} from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { NavLink } from "react-router-dom";
+
 const Header = () => {
-    return (
-        <>
+  const [showMediaIcons, setShowMediaIcons] = useState(false);
+  return (
+    <>
+      <nav className="main-nav">
+        {/* 1st logo part  */}
+        <div className="logo">
+          <h2>
+            <span>M</span>anish
+            <span>K</span>umar
+          </h2>
+        </div>
 
-            <div className='nav-items'>
-                <ul className="snip1226">
-                    <Link to="/">
-                        <li className='nav-items current' data-hover="Home">Home</li>
-                    </Link>
-                    <Link to="./Projects">
-                        <li className='nav-items' data-hover="Products">Projects</li>
-                    </Link>
-                    <Link to="/about">
-                        <li className='nav-items' data-hover="About Us">About me</li>
-                    </Link>
-                    <Link to="/contact">
-                        <li className='nav-items' data-hover="Contact">Contact</li>
-                    </Link>
-                </ul>
-            </div>
+        {/* 2nd menu part  */}
+        <div
+          className={
+            showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"
+          }>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/Projects">Projects</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About me</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">contact</NavLink>
+            </li>
+          </ul>
+        </div>
 
+        {/* 3rd social media links */}
+        <div className="social-media">
+          <ul className="social-media-desktop">
+            <li>
+              <a
+                href="https://www.youtube.com/channel/UCwfaAHy4zQUb2APNOGXUCCA"
+                target="_thapa">
+                <FaFacebookSquare className="facebook" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com/thapatechnical/"
+                target="_thapa">
+                <FaInstagramSquare className="instagram" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.youtube.com/channel/UCwfaAHy4zQUb2APNOGXUCCA"
+                target="_thapa">
+                <FaYoutubeSquare className="youtube" />
+              </a>
+            </li>
+          </ul>
 
-            {/* <ul class="snip1226">
-  <li class="current"><a href="/" data-hover="Home">Home</a></li>
-  <li><a href="/" data-hover="About Us">About Us</a></li>
-  <li><a href="/" data-hover="Blog">Blog</a></li>
-  <li><a href="/" data-hover="Services">Services</a></li>
-  <li><a href="/" data-hover="Products">Products</a></li>
-  <li><a href="/" data-hover="Contact">Contact</a></li>
-</ul> */}
-        </>
-    );
+          {/* hamburget menu start  */}
+          <div className="hamburger-menu">
+            <a href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
+              <GiHamburgerMenu />
+            </a>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 };
 export default Header;
