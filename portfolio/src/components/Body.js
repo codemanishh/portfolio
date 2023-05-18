7b3967652757bd4cec7128882a4c65e7d7d37b0f
manish <rrew1212@gmail.com>
2023-05-19 00:50:26 +0530

    half main section done

--- portfolio/src/components/Body.js
+++ portfolio/src/components/Body.js
@@ -1,19 +1,40 @@
 import React from 'react'
-
+import "./Body.css";
+import {
+  FaFacebookSquare,
+  FaInstagramSquare,
+  FaYoutubeSquare,
+} from "react-icons/fa";
 const Body = () => {
   return (
-    <>
-      <div className="card" style={{width: '18rem'}}>
-        <img src="https://images.unsplash.com/photo-1682685796766-0fddd3e480de?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDd8Ym84alFLVGFFMFl8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" className="card-img-top" alt="..."/>
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="https://youtu.be/cHHU0jXfjKY" className="btn btn-primary">Go somewhere</a>
-          </div>
-      </div>
+    <div className='main-class'>
 
-    </>
+        <ul className="social-media-desktop-a">
+          <li>
+            <a
+              href="https://www.youtube.com/channel/UCwfaAHy4zQUb2APNOGXUCCA"
+              target="_thapa">
+              <FaFacebookSquare className="facebook logo-s" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/thapatechnical/"
+              target="_thapa">
+              <FaInstagramSquare className="instagram logo-s" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.youtube.com/channel/UCwfaAHy4zQUb2APNOGXUCCA"
+              target="_thapa">
+              <FaYoutubeSquare className="youtube logo-s" />
+            </a>
+          </li>
+        </ul>
+    </div>
   )
 }
 
 export default Body
+
--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -1,10 +1,5 @@
 import React, { useState } from "react";
 import "./Header.css";
-import {
-  FaFacebookSquare,
-  FaInstagramSquare,
-  FaYoutubeSquare,
-} from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { NavLink } from "react-router-dom";
 
@@ -41,34 +36,7 @@ const Header = () => {
             </li>
           </ul>
         </div>
-
-        {/* 3rd social media links */}
         <div className="social-media">
-          <ul className="social-media-desktop">
-            <li>
-              <a
-                href="https://www.youtube.com/channel/UCwfaAHy4zQUb2APNOGXUCCA"
-                target="_thapa">
-                <FaFacebookSquare className="facebook" />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.instagram.com/thapatechnical/"
-                target="_thapa">
-                <FaInstagramSquare className="instagram" />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.youtube.com/channel/UCwfaAHy4zQUb2APNOGXUCCA"
-                target="_thapa">
-                <FaYoutubeSquare className="youtube" />
-              </a>
-            </li>
-          </ul>
-
-          {/* hamburget menu start  */}
           <div className="hamburger-menu">
             <a href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
               <GiHamburgerMenu />
