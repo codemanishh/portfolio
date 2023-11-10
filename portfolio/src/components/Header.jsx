0bbb2d9741a945dd6d43a2fc3fe6cac481c25905
manish <rrew1212@gmail.com>
2023-11-10 10:30:12 +0530

    scroll in nav added

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -2,41 +2,45 @@ import React, { useState } from "react";
 import "./Header.css";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { Link } from "react-router-dom";
-import img1 from './SlillCards/img/nodejs.png'
+import img1 from './SlillCards/img/nodejs.png';
 
 const Header = () => {
   const [showMediaIcons, setShowMediaIcons] = useState(false);
+
+  // Smooth scrolling function
+  const handleSmoothScroll = (id) => {
+    const element = document.getElementById(id);
+    element.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <>
       <nav className="main-nav">
         {/* 1st logo part  */}
         <div className="logo">
-         <img src={img1} style={{width:'30px'}} alt="" />
+          <img src={img1} style={{ width: '30px' }} alt="" />
         </div>
 
         {/* 2nd menu part  */}
-        <div
-          className={
-            showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"
-          }>
+        <div className={showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"}>
           <ul>
             <li>
-              <a href="#home"  >Home</a>
+              <Link to="#" onClick={() => handleSmoothScroll('home')}>Home</Link>
             </li>
             <li>
-              <a href="#projects">Projects</a>
+              <Link to="#" onClick={() => handleSmoothScroll('projects')}>Projects</Link>
             </li>
             <li>
-              <a href="#about" >About me</a>
+              <Link to="#" onClick={() => handleSmoothScroll('about')}>About me</Link>
             </li>
             <li>
-              <a href="#contact1">contact</a>
+              <Link to="#" onClick={() => handleSmoothScroll('contact1')}>Contact</Link>
             </li>
           </ul>
         </div>
         <div className="social-media">
           <div className="hamburger-menu">
-            <Link to="#" onClick={() => setShowMediaIcons(!showMediaIcons)} >
+            <Link to="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
               <GiHamburgerMenu />
             </Link>
           </div>
